ed90f4686cb8a392bdb24682b21a083a4935ff44
Samed Yalniz <yalniz.samed@gmail.com>
2020-07-23 07:24:37 +0200

    adding form validation

--- script.js
+++ script.js
@@ -18,3 +18,40 @@ function showModal(){
 modalShow.addEventListener('click', showModal);
 modalClose.addEventListener('click',()=> modal.classList.remove('show-modal'));
 window.addEventListener('click', (event) => (event.target=== modal ? modal.classList.remove('show-modal') : false));
+
+// Validate Form
+function validateForm(nameValue, urlValue){
+    const urlExpression = /https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)/g;
+    const regex = new RegExp(urlExpression);
+    if(!nameValue || !urlValue){
+        alert('Please submit values for both fields.');
+        return false;
+    }
+    if(!urlValue.match(regex)){
+        alert('Please provide a valid web address')
+        return false;
+    }
+    //Valid
+    return true;
+
+
+}
+
+// Handle Data from Form
+function storeBookmark(event){
+    event.preventDefault();
+    const nameValue = websiteNameEl.value;
+    let urlValue = websiteUrlEl.value;
+    if(!urlValue.includes('http://', 'https://')) {
+        urlValue = `https://${urlValue}`;
+    }
+
+    console.log(nameValue, urlValue);
+    validateForm(nameValue, urlValue);
+    if(!validateForm(nameValue, urlValue)) {
+        return false;
+    }
+}
+
+// Event Listener
+bookmarkForm.addEventListener('submit', storeBookmark);
